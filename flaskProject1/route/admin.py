7c9e5f839768ed291a2082c820ab9757ff4c62a0
seojonghon <seojonghon@naver.com>
2023-12-26 18:16:47 +0900

    최종관리자 삭제기능 추가 폴더 분리완료

--- flaskProject1/route/admin.py
+++ flaskProject1/route/admin.py
@@ -24,3 +24,26 @@ def admin_page():
     except Exception as e:
         print("Error fetching user data:", e)
         return jsonify({"message": "Error fetching user data"}), 500
+
+
+@admin_bp.route('/editor/customer/<email>', methods=['DELETE'])
+def delete_customer(email):
+    try:
+        with pymysql.connect(**database.connectionString) as con:
+            cursor = con.cursor()
+
+            # 스키마 변경
+            cursor.execute("USE editor;")
+
+            # 테이블명 가져오기
+            table_name = 'customer'
+
+            # 사용자 삭제
+            sql = f"DELETE FROM {table_name} WHERE email = %s;"
+            cursor.execute(sql, (email,))
+            con.commit()
+
+            return jsonify({"message": f"사용자 {email}이(가) 성공적으로 삭제되었습니다."}), 200
+    except Exception as e:
+        print("사용자 삭제 중 오류 발생:", e)
+        return jsonify({"message": "사용자 삭제 중 오류가 발생했습니다."}), 500
