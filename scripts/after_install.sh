c205d0ce302900a32d5eb82b0c569d3493dca4dd
seojonghon <seojonghon@naver.com>
2024-01-03 19:07:11 +0900

    최종수정중 배포

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -9,4 +9,4 @@ touch /home/ubuntu/ssgAdminBE/log.txt
 sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
 
 echo ">>> start server ---------------------"
-gunicorn --bind 0.0.0.0:5000 --timeout 90 "app:create_app()" > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
+gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
