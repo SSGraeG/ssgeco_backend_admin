6b6aa7b2d18d7f6bb120732d748980c7a925dbfa
seojonghon <seojonghon@naver.com>
2024-01-03 17:41:02 +0900

    최종수정중 배포

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 var=$(ps -ef | grep 'gunicorn' | grep -v 'grep')
-pid1=$(echo ${var} | cut -d " " -f2)
-pid2=$(echo ${var} | cut -d " " -f16)
+pid1=$(echo "${var}" | awk '{print $2}')
+pid2=$(echo "${var}" | awk '{print $16}')
 
-if [ -n "${pid1}" ] && [ -n "${pid2}" ];
+if [ -n "${pid1}" -a -n "${pid2}" ];
 then
-    sudo kill -9 ${pid1}
-    sudo kill -9 ${pid2}
-    echo "${pid1} and ${pid2} are terminated."
+    sudo kill -9 "${pid1}"
+    sudo kill -9 "${pid2}"
+    echo "${pid1}와 ${pid2}가 종료되었습니다."
 else
-    echo "gunicorn processes are not running."
+    echo "gunicorn 프로세스가 실행되고 있지 않습니다."
 fi
 
 rm -rf /home/ubuntu/gunicorn.log
-
 rm -rf /home/ubuntu/ssgAdminBE
-mkdir  /home/ubuntu/ssgAdminBE
+mkdir /home/ubuntu/ssgAdminBE
