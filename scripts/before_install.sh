874f07c31c5aa487147fa6663d88cf6a4f4b3840
seojonghon <seojonghon@naver.com>
2024-01-03 19:34:15 +0900

    최종수정중 배포

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -10,8 +10,6 @@ chown -R ubuntu /home/ubuntu/ssgAdminBE
 # 기존에 실행 중인 gunicorn 프로세스를 종료합니다.
 pkill gunicorn
 
-# Wait for existing processes to be terminated
-sleep 5
 
 # 로그 파일과 프로젝트 디렉터리를 초기화합니다.
 rm -rf /home/ubuntu/gunicorn.log
