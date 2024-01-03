cc5911a538550f929aee2828a8fd9733ba329b03
seojonghon <seojonghon@naver.com>
2024-01-03 18:37:57 +0900

    최종수정중 배포

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,9 +6,6 @@ echo ">>> pip install ----------------------"
 pip install -r requirements.txt
 pip install flask-cors  # Install flask-cors explicitly
 
-echo ">>> remove template files ------------"
-rm -rf appspec.yml requirements.txt
-
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/ssgAdminBE
 
