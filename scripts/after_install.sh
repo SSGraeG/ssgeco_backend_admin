23d4249e10970df2ad2ba985de9e6a85be46470c
seojonghon <seojonghon@naver.com>
2024-01-12 17:24:44 +0900

    깃액션 3 쉘스크립트 수정

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# after_install.sh
-
-# 원격 서버에서 실행할 명령들
+# 베스천 호스트에서 프라이빗 서브넷의 EC2로 코드를 복사하고 스크립트 실행
 ssh -i "adminBE.pem" ubuntu@10.0.132.46 << 'ENDSSH'
-  # before_install.sh의 내용
-  # ...
+  # 프라이빗 서브넷의 EC2에서 스크립트 실행
+  ssh -i "adminBE.pem" ubuntu@private-subnet-ip << 'INNER_SSH'
+    cd /home/ubuntu/ssgAdminBE
 
-  echo ">>> pip install ----------------------"
-  pip install -r requirements.txt
-  pip install multidict
-  echo ">>> change owner to ubuntu -----------"
-  chown -R ubuntu /home/ubuntu/ssgAdminBE
+    echo ">>> pip install ----------------------"
+    pip install -r requirements.txt
+    pip install multidict
+    echo ">>> change owner to ubuntu -----------"
+    chown -R ubuntu /home/ubuntu/ssgAdminBE
 
-  touch /home/ubuntu/ssgAdminBE/log.txt
+    touch /home/ubuntu/ssgAdminBE/log.txt
 
-  sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
+    sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
 
-  echo ">>> start server ---------------------"
-  gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
+    echo ">>> start server ---------------------"
+    nohup gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
+  INNER_SSH
 ENDSSH
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# before_install.sh
-
-# SSH로 원격 서버에 접속하고 코드를 전송하는 부분
+# 코드를 베스천 호스트로 복사
 scp -i "adminBE.pem" -r /path/to/local/code ubuntu@10.0.132.46:/home/ubuntu/ssgAdminBE
 
-# 원격 서버에서 실행할 명령들
+# 베스천 호스트에서 프라이빗 서브넷의 EC2로 코드를 복사하고 스크립트 실행
 ssh -i "adminBE.pem" ubuntu@10.0.132.46 << 'ENDSSH'
   # 이전에 실행된 gunicorn 프로세스를 중지합니다.
   pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app'
@@ -15,5 +13,11 @@ ssh -i "adminBE.pem" ubuntu@10.0.132.46 << 'ENDSSH'
   rm -rf /home/ubuntu/ssgAdminBE
   mkdir /home/ubuntu/ssgAdminBE
 
-  cd /home/ubuntu/ssgAdminBE
+  # 코드를 프라이빗 서브넷의 EC2로 복사
+  scp -r /home/ubuntu/ssgAdminBE ubuntu@private-subnet-ip:/home/ubuntu
+
+  # 프라이빗 서브넷의 EC2에서 스크립트 실행
+  ssh -i "adminBE.pem" ubuntu@private-subnet-ip << 'INNER_SSH'
+    cd /home/ubuntu/ssgAdminBE
+  INNER_SSH
 ENDSSH
