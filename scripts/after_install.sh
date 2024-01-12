54a7670aa73e3e35c732f527c06c587b53a19857
seojonghon <seojonghon@naver.com>
2024-01-12 17:16:46 +0900

    깃액션 2 쉘스크립트 수정

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-cd /home/ubuntu/ssgAdminBE
+# after_install.sh
 
-echo ">>> pip install ----------------------"
-pip install -r requirements.txt
-pip install multidict
-echo ">>> change owner to ubuntu -----------"
-chown -R ubuntu /home/ubuntu/ssgAdminBE
+# 원격 서버에서 실행할 명령들
+ssh -i "adminBE.pem" ubuntu@10.0.132.46 << 'ENDSSH'
+  # before_install.sh의 내용
+  # ...
 
+  echo ">>> pip install ----------------------"
+  pip install -r requirements.txt
+  pip install multidict
+  echo ">>> change owner to ubuntu -----------"
+  chown -R ubuntu /home/ubuntu/ssgAdminBE
 
-cd /home/ubuntu/ssgAdminBE
+  touch /home/ubuntu/ssgAdminBE/log.txt
 
-touch /home/ubuntu/ssgAdminBE/log.txt
+  sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
 
-sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
-
-echo ">>> start server ---------------------"
-gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
+  echo ">>> start server ---------------------"
+  gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
+ENDSSH
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-ssh -i "adminBE.pem" ubuntu@10.0.132.46
+# before_install.sh
 
-# 이전에 실행된 gunicorn 프로세스를 중지합니다.
-pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app'
+# SSH로 원격 서버에 접속하고 코드를 전송하는 부분
+scp -i "adminBE.pem" -r /path/to/local/code ubuntu@10.0.132.46:/home/ubuntu/ssgAdminBE
 
-# 로그 파일과 프로젝트 디렉터리를 초기화합니다.
-rm -rf /home/ubuntu/gunicorn.log
-rm -rf /home/ubuntu/ssgAdminBE
-mkdir /home/ubuntu/ssgAdminBE
+# 원격 서버에서 실행할 명령들
+ssh -i "adminBE.pem" ubuntu@10.0.132.46 << 'ENDSSH'
+  # 이전에 실행된 gunicorn 프로세스를 중지합니다.
+  pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app'
 
-cd /home/ubuntu/ssgAdminBE
+  # 로그 파일과 프로젝트 디렉터리를 초기화합니다.
+  rm -rf /home/ubuntu/gunicorn.log
+  rm -rf /home/ubuntu/ssgAdminBE
+  mkdir /home/ubuntu/ssgAdminBE
+
+  cd /home/ubuntu/ssgAdminBE
+ENDSSH
