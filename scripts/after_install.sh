f7f7e353dfe289dcd941e9dcc723936cf6681c9b
seojonghon <seojonghon@naver.com>
2024-01-12 17:51:09 +0900

    깃액션 4 쉘스크립트 수정

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # 베스천 호스트에서 프라이빗 서브넷의 EC2로 코드를 복사하고 스크립트 실행
-ssh -i "adminBE.pem" ubuntu@10.0.132.46 << 'ENDSSH'
-  # 프라이빗 서브넷의 EC2에서 스크립트 실행
-  ssh -i "adminBE.pem" ubuntu@private-subnet-ip << 'INNER_SSH'
-    cd /home/ubuntu/ssgAdminBE
+# 프라이빗 서브넷의 EC2에서 스크립트 실행
+cd /home/ubuntu/ssgAdminBE
 
-    echo ">>> pip install ----------------------"
-    pip install -r requirements.txt
-    pip install multidict
-    echo ">>> change owner to ubuntu -----------"
-    chown -R ubuntu /home/ubuntu/ssgAdminBE
+echo ">>> pip install ----------------------"
+pip install -r requirements.txt
+pip install multidict
+echo ">>> change owner to ubuntu -----------"
+chown -R ubuntu /home/ubuntu/ssgAdminBE
 
-    touch /home/ubuntu/ssgAdminBE/log.txt
+touch /home/ubuntu/ssgAdminBE/log.txt
 
-    sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
+sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
 
-    echo ">>> start server ---------------------"
-    nohup gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
-  INNER_SSH
-ENDSSH
+echo ">>> start server ---------------------"
+nohup gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-
-# 코드를 베스천 호스트로 복사
-scp -i "adminBE.pem" -r /path/to/local/code ubuntu@10.0.132.46:/home/ubuntu/ssgAdminBE
-
 # 베스천 호스트에서 프라이빗 서브넷의 EC2로 코드를 복사하고 스크립트 실행
-ssh -i "adminBE.pem" ubuntu@10.0.132.46 << 'ENDSSH'
-  # 이전에 실행된 gunicorn 프로세스를 중지합니다.
-  pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app'
+ssh -i "adminBE.pem" ubuntu@10.0.132.46
+# 이전에 실행된 gunicorn 프로세스를 중지합니다.
+pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app'
 
-  # 로그 파일과 프로젝트 디렉터리를 초기화합니다.
-  rm -rf /home/ubuntu/gunicorn.log
-  rm -rf /home/ubuntu/ssgAdminBE
-  mkdir /home/ubuntu/ssgAdminBE
+# 로그 파일과 프로젝트 디렉터리를 초기화합니다.
+rm -rf /home/ubuntu/gunicorn.log
+rm -rf /home/ubuntu/ssgAdminBE
+mkdir /home/ubuntu/ssgAdminBE
 
-  # 코드를 프라이빗 서브넷의 EC2로 복사
-  scp -r /home/ubuntu/ssgAdminBE ubuntu@private-subnet-ip:/home/ubuntu
+# 코드를 프라이빗 서브넷의 EC2로 복사
+scp -r /home/ubuntu/ssgAdminBE ubuntu@10.0.132.46:/home/ubuntu
 
-  # 프라이빗 서브넷의 EC2에서 스크립트 실행
-  ssh -i "adminBE.pem" ubuntu@private-subnet-ip << 'INNER_SSH'
-    cd /home/ubuntu/ssgAdminBE
-  INNER_SSH
-ENDSSH
+# 프라이빗 서브넷의 EC2에서 스크립트 실행
+ssh -i "adminBE.pem" ubuntu@10.0.132.46
+cd /home/ubuntu/ssgAdminBE
+
