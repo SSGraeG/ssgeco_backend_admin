673230fcee6bb2f9339f9882eb25c3320154a0fe
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2024-01-15 11:42:17 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,13 +8,17 @@ pip install multidict
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/ssgAdminBE
 
-
 cd /home/ubuntu/ssgAdminBE
 
 touch /home/ubuntu/ssgAdminBE/log.txt
 
-sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
-touch /home/ubuntu/gunicorn.log && chmod +w /home/ubuntu/gunicorn.log
+# 로그 파일의 소유자 변경
+sudo chown ubuntu:ubuntu /home/ubuntu/gunicorn.log
+touch /home/ubuntu/gunicorn.log
 
 echo ">>> start server ---------------------"
-gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
+# 권한 설정을 추가하고, 로그 파일을 기록 가능하도록 수정
+gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /home/ubuntu/gunicorn.log 2>&1 &
+
+# 서버가 정상적으로 실행되었는지 확인하기 위해 로그를 출력
+tail -f /home/ubuntu/gunicorn.log
