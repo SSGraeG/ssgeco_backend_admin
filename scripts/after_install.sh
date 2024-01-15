9e5c7f2084dce5a0c68dbc236874359b3177dfab
seojonghon <seojonghon@naver.com>
2024-01-15 11:12:23 +0900

    깃액션 6 프라이빗 서브넷으로 바로 배포

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-LOGFILE=/home/ubuntu/gunicorn.log
 
-cd /home/ubuntu/ssgAdminBE || exit
+cd /home/ubuntu/ssgAdminBE
 
-echo ">>> pip install"
+echo ">>> pip install ----------------------"
 pip install -r requirements.txt
-
-echo ">>> remove template files "
-rm -rf appspec.yml requirements.txt
-
-echo ">>> change owner to ubuntu "
+pip install multidict
+echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/ssgAdminBE
 
-sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE
+
+cd /home/ubuntu/ssgAdminBE
+
+touch /home/ubuntu/ssgAdminBE/log.txt
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
 
 echo ">>> start server ---------------------"
-gunicorn --bind 0.0.0.0:5000 --timeout 90 --log-level=debug "app:create_app()" >> "$LOGFILE" 2>&1 &
+gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # 이전에 실행된 gunicorn 프로세스를 중지합니다.
-pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:create_app()'
+pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app'
 
 # 로그 파일과 프로젝트 디렉터리를 초기화합니다.
 rm -rf /home/ubuntu/gunicorn.log
+
 rm -rf /home/ubuntu/ssgAdminBE
 mkdir /home/ubuntu/ssgAdminBE
 
