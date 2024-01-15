295cba9a134afd0bb7f66c95777da847bcebfd77
seojonghon <seojonghon@naver.com>
2024-01-15 11:25:48 +0900

    깃액션 6 프라이빗 서브넷으로 바로 배포

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -14,6 +14,7 @@ cd /home/ubuntu/ssgAdminBE
 touch /home/ubuntu/ssgAdminBE/log.txt
 
 sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
+touch /home/ubuntu/gunicorn.log && chmod +w /home/ubuntu/gunicorn.log
 
 echo ">>> start server ---------------------"
 gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
