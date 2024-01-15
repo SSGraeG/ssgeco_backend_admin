d4c60ad29bbb257c7c6179d999b0190a0b13717d
seojonghon <seojonghon@naver.com>
2024-01-15 12:04:33 +0900

    깃액션 6 프라이빗 서브넷으로 바로 배포

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -9,18 +9,17 @@ echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/ssgAdminBE
 
 pip install sshtunnel
+pip install multidict
 
 cd /home/ubuntu/ssgAdminBE
 
 touch /home/ubuntu/ssgAdminBE/log.txt
 
 # 로그 파일의 소유자 변경
-sudo chown ubuntu:ubuntu /home/ubuntu/gunicorn.log
 touch /home/ubuntu/gunicorn.log
+sudo chown ubuntu:ubuntu /home/ubuntu/gunicorn.log
+
 
 echo ">>> start server ---------------------"
 # 권한 설정을 추가하고, 로그 파일을 기록 가능하도록 수정
-gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /home/ubuntu/gunicorn.log 2>&1 &
-
-# 서버가 정상적으로 실행되었는지 확인하기 위해 로그를 출력
-tail -f /home/ubuntu/gunicorn.log
+gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
