d64f95dd4fcbf4d4f3c89786a980ba73940308a4
seojonghon <seojonghon@naver.com>
2024-01-15 09:26:35 +0900

    깃액션 5 프라이빗 서브넷으로 바로 배포

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+LOGFILE=/home/ubuntu/gunicorn.log
 
-# 베스천 호스트에서 프라이빗 서브넷의 EC2로 코드를 복사하고 스크립트 실행
-# 프라이빗 서브넷의 EC2에서 스크립트 실행
-cd /home/ubuntu/ssgAdminBE
+cd /home/ubuntu/ssgAdminBE || exit
 
-echo ">>> pip install ----------------------"
+echo ">>> pip install"
 pip install -r requirements.txt
-pip install multidict
-echo ">>> change owner to ubuntu -----------"
+
+echo ">>> remove template files "
+rm -rf appspec.yml requirements.txt
+
+echo ">>> change owner to ubuntu "
 chown -R ubuntu /home/ubuntu/ssgAdminBE
 
-touch /home/ubuntu/ssgAdminBE/log.txt
-
-sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE/
+sudo chown -R ubuntu:ubuntu /home/ubuntu/ssgAdminBE
 
 echo ">>> start server ---------------------"
-nohup gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app > /dev/null 2> /home/ubuntu/gunicorn.log </dev/null &
+gunicorn --bind 0.0.0.0:5000 --timeout 90 --log-level=debug "app:create_app()" >> "$LOGFILE" 2>&1 &
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-# 베스천 호스트에서 프라이빗 서브넷의 EC2로 코드를 복사하고 스크립트 실행
-ssh -i "adminBE.pem" ubuntu@10.0.132.46
+
 # 이전에 실행된 gunicorn 프로세스를 중지합니다.
-pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app'
+pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:create_app()'
 
 # 로그 파일과 프로젝트 디렉터리를 초기화합니다.
 rm -rf /home/ubuntu/gunicorn.log
 rm -rf /home/ubuntu/ssgAdminBE
 mkdir /home/ubuntu/ssgAdminBE
 
-# 코드를 프라이빗 서브넷의 EC2로 복사
-scp -r /home/ubuntu/ssgAdminBE ubuntu@10.0.132.46:/home/ubuntu
-
-# 프라이빗 서브넷의 EC2에서 스크립트 실행
-ssh -i "adminBE.pem" ubuntu@10.0.132.46
 cd /home/ubuntu/ssgAdminBE
-
