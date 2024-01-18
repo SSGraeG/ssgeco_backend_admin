700489c5839f11f91577cbb1966eb9df133af974
hwan515 <stophwan515@gmail.com>
2024-01-18 17:18:50 +0900

    스크립트 파일 및 re수정

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
+LOGFILE=/home/ubuntu/gunicorn.log
 
 cd /home/ubuntu/ssgAdminBE
 
 echo ">>> pip install ----------------------"
 pip install -r requirements.txt
-pip install multidict
+
+echo ">>> remove template files "
+rm -rf appspec.yml requirements.txt
+
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/ssgAdminBE
 
-pip install sshtunnel
-pip install multidict
-
-cd /home/ubuntu/ssgAdminBE
-
-touch /home/ubuntu/ssgAdminBE/log.txt
-
-# 로그 파일의 소유자 변경
-touch /home/ubuntu/flasktest.log
-sudo chown ubuntu:ubuntu /home/ubuntu/flasktest.log
+sudo chown ubuntu:ubuntu /home/ubuntu/ssgAdminBE
 
 echo ">>> start server ---------------------"
-# Flask 내장 서버로 실행 (실제 운영에서는 Gunicorn 권장)
-flask run --host=0.0.0.0 --port=5000 > /dev/null 2> /home/ubuntu/flasktest.log </dev/null &
+gunicorn --bind 0.0.0.0:5000 --timeout 90 app:app >> "$LOGFILE" 2>&1 &
